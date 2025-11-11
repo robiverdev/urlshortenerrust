0af8d609515055adbac04106c9e939bbb27cb0d5
robiverdev <robi.verdev1@gmail.com>
2025-11-11 19:28:33 +0100

    initial actix web server setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web,App,HttpServer, HttpResponse};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server running on localhost:4000");
+
+    HttpServer::new(|| {
+        App::new().route("/", web::get().to(|| async {
+            HttpResponse::Ok().body("URL Shortener / route")
+        }))
+    }).bind(("localhost", 4000))?.run().await
 }
